01d9fd5796da1f3bc7d47a709600884f4b23b4e4
Joel Purra <code+github@joelpurra.com>
2014-05-14 10:49:30 +0200

    Break out jq variable, simplify

--- src/extract/request/url.sh
+++ src/extract/request/url.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 set -e
 
+read -d '' getRequestUrls <<-'EOF' || true
+.log.entries | map(.request.url)
+EOF
+
 while read harpath;
 do
-	cat "$harpath" | jq '[ .log.entries[].request.url ]'
+	cat "$harpath" | jq "$getRequestUrls"
 done
