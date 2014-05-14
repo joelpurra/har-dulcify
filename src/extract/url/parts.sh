2b214bf1b1cd9edd3519e77276c0c75a97902d3a
Joel Purra <code+github@joelpurra.com>
2014-05-14 11:00:04 +0200

    Split URLs into parts

---
+++ src/extract/url/parts.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e
+
+read -d '' splitUrlToParts <<-'EOF' || true
+def splitUrlToParts:
+	split("://") as $protocolParts
+		| {
+			url: .,
+			protocol: $protocolParts[0],
+			domain: ($protocolParts[1] | split("/")[0])
+		};
+
+[
+	.[]
+	| if (. | type) == "string" then
+		splitUrlToParts
+	else
+		empty
+	end
+]
+EOF
+
+cat | jq "$splitUrlToParts"
