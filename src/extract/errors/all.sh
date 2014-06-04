4882eb1f09e3f2cb6ef536bf5021e80e5d3c2676
Joel Purra <code+github@joelpurra.com>
2014-06-04 15:12:41 +0200

    Look at errors in individual entries for comments (errors)

--- src/extract/errors/all.sh
+++ src/extract/errors/all.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 set -e
 
+read -d '' allWithComments <<-'EOF' || true
+select(
+	(.log.comment | length > 0)
+	or (
+		reduce .log.entries[] as $entry
+			(
+				false;
+				. or ($entry | (.response.comment | length) > 0)
+			)
+	)
+)
+EOF
+
 while read harpath;
 do
-	cat "$harpath" | jq '[ . | select(.log.comment | length > 0) ]' || (echo "ERROR: $harpath" >/dev/stderr)
+	cat "$harpath" | jq "$allWithComments" || (echo "ERROR: $harpath" >/dev/stderr)
 done
---
+++ src/extract/errors/page.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -e
+
+while read harpath;
+do
+	cat "$harpath" | jq 'select(.log.comment | length > 0)' || (echo "ERROR: $harpath" >/dev/stderr)
+done
