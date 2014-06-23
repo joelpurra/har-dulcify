bae188f83bc345ff3cd599c6f0381fd6e1fc66b9
Joel Purra <code+github@joelpurra.com>
2014-06-23 13:41:01 +0200

    Rename to reflect basic classifications

---
+++ src/classification/basic.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+set -e
+
+read -d '' classifyExpandedParts <<-'EOF' || true
+def isSameDomain(domain):
+	domain as $domain
+	| . == $domain;
+
+def isSubdomain(domain):
+	domain as $domain
+	| endswith("." + $domain);
+
+def isSecure:
+	. == "https";
+
+def classifyUrl(origin):
+	origin as $origin
+	| {
+		isSameDomain: (.domain | isSameDomain($origin.domain)),
+		isSubdomain: (.domain | isSubdomain($origin.domain)),
+		isSecure: (.protocol | isSecure)
+	};
+
+def mangle(origin):
+	origin as $origin
+	| .url as $urlParts
+	| . + {
+		classification : $urlParts | classifyUrl($origin)
+	};
+
+.origin.url as $origin
+| {
+	origin: .origin | mangle($origin),
+	requestedUrls: .requestedUrls | map(mangle(($origin)))
+}
+EOF
+
+cat | jq "$classifyExpandedParts"
