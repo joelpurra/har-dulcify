8309047501f1b43a3bfaf11f0cd4d6c8c709a019
Joel Purra <code+github@joelpurra.com>
2014-06-24 14:54:44 +0200

    Transform disconnect's services.json file to a format that's faster to make lookups against, with speedups of about 99.6%

---
+++ src/classification/disconnect/add.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+set -e
+
+disconnectClassificationFile="$1"
+
+read -d '' classifyExpandedParts <<-'EOF' || true
+def deleteNullKey(key):
+	# Delete a property if it is null.
+	key as $key
+	| with_entries(
+		select(
+			.key != $key
+			or (
+				.key == $key
+				and
+				(.value | type) != "null"
+			)
+		)
+	);
+
+def deleteEmptyArrayKey(key):
+	# Delete a property if it is an empty array.
+	key as $key
+	| with_entries(
+		select(
+			.key != $key
+			or (
+				.key == $key
+				and
+				(.value | length) != 0
+			)
+		)
+	);
+
+def matchDisconnect:
+	# Match the domain to disconnect's list.
+	# If the domain is a subdomain of a domain in disconnect's list, include it too.
+	. as $domain
+	| ($domain | split(".")) as $domainParts
+	# Negative range to build the domain from parts from the right.
+	| [ range((($domainParts | length) * -1); -1) ]
+	| map(
+		# Assemble the domain, longest domain combination first.
+		($domainParts[.:] | join(".")) as $subdomain
+		| if $disconnect | has($subdomain) then
+			(
+				# Inject the matched service domain into the returned object.
+				{
+					domain: $subdomain
+				}
+				+ $disconnect[$subdomain]
+			)
+		else
+			empty
+		end
+	)
+	# In case a subdomain matches more than once, keep only one of each match.
+	| unique;
+
+def mangle:
+	.blocks += ({
+			disconnect: .url.domain | matchDisconnect
+		}
+		| deleteEmptyArrayKey("disconnect"))
+	| deleteNullKey("blocks");
+
+{
+	origin: .origin | mangle,
+	requestedUrls: .requestedUrls | map(mangle)
+}
+EOF
+
+cat | jq "$classifyExpandedParts" --argfile "disconnect" "$disconnectClassificationFile"
---
+++ src/classification/disconnect/prepare-service-list.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+set -e
+
+read -d '' classifyExpandedParts <<-'EOF' || true
+def toNullOrSingleValueOrArray:
+	if length == 0 then
+		# Replace an empty array with null.
+		null
+	elif length == 1 then
+		# Replace an array with a single element with that element.
+		.[0]
+	else
+		# Return an array with more than one element as is.
+		.
+	end;
+
+def flattenDisconnect:
+	# services.json keeps service domains in the deepest level.
+	# This flattens the hierarchy to one object per domain, but duplicates other information.
+	# Done to be able to perform hash/object-has-property lookups on domains.
+	[
+		to_entries
+		| .[]
+		| .key as $category
+		| .value[]
+		| to_entries
+		| .[]
+		| .key as $organization
+		| .value
+		| to_entries
+		| .[]
+		| .key as $url
+		| .value[]
+		| . as $domain
+		| {
+			domain: $domain,
+			url: $url,
+			organization: $organization,
+			category: $category,
+		}
+	];
+
+def groupDomains:
+	group_by(.domain)
+	| map(
+		{
+			# Create a property out of the domain.
+			(.[0].domain): {
+				# Merge deeper properties to (arrays of) unique values.
+				# NOTE: at the time of writing only four domains in services.json have more than one value for category/organization/url, but keeping them in arrays just in case.
+				urls: [ .[].url ] | unique | toNullOrSingleValueOrArray,
+				organizations: [ .[].organization ] | unique | toNullOrSingleValueOrArray,
+				categories: [ .[].category ] | unique | toNullOrSingleValueOrArray
+			}
+		}
+	);
+
+def mergeArrayOfObjectsToObject:
+	# Assumes that the array's objects have unique enough properties to be suitable for merging.
+	reduce .[] as $obj ({}; . + $obj);
+
+def transformRawDisconnect:
+	flattenDisconnect
+	| groupDomains
+	| mergeArrayOfObjectsToObject;
+
+.categories
+# Too many domains are duplicated in the legacy lists.
+| del(."Legacy Content")
+| del(."Legacy Disconnect")
+| transformRawDisconnect
+EOF
+
+cat | jq "$classifyExpandedParts"
