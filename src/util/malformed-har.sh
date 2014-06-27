fcfec60c5d7f8abf1cc0f67493cad0add5ba6a69
Joel Purra <code+github@joelpurra.com>
2014-06-27 11:11:42 +0200

    Find malformed HAR files from a list of paths

---
+++ src/util/malformed-har.sh
@@ -0,0 +1,15 @@
+ #!/usr/bin/env bash
+set -e
+
+while read harpath;
+do
+	# Let jq parse the entire file, but discard output
+	set +e
+	cat "$harpath" | jq '.' >/dev/null
+	jqExitCode=$?
+	set -e
+
+	if [[ $jqExitCode != 0 ]]; then
+		echo "$harpath"
+	fi
+done
