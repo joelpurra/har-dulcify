313d0070c0cf93586f28708d9dd10f9d1daf1aec
Joel Purra <code+github@joelpurra.com>
2014-07-02 15:04:36 +0200

    Download disconnect's services.json as part of the preparations

--- src/one-shot/preparations.sh
+++ src/one-shot/preparations.sh
@@ -1,4 +1,7 @@
  #!/usr/bin/env bash
 set -e
 
-cat "services.json" | "${BASH_SOURCE%/*}/../classification/disconnect/prepare-service-list.sh" > "prepared.disconnect.services.json"
+# services.json (also known as disconnect-plaintext.json) from disconnect.me
+# Alternative URL: https://github.com/disconnectme/disconnect/raw/master/firefox/content/disconnect.safariextension/opera/chrome/data/services.json
+wget "https://services.disconnect.me/disconnect-plaintext.json"
+cat "disconnect-plaintext.json" | "${BASH_SOURCE%/*}/../classification/disconnect/prepare-service-list.sh" > "prepared.disconnect.services.json"
