84ec087020192b5f45a6aea2a24778f9df64cc5d
Joel Purra <code+github@joelpurra.com>
2014-07-16 15:05:03 +0200

    Only look for .har files

--- src/domains/latest/single.sh
+++ src/domains/latest/single.sh
@@ -5,7 +5,7 @@ domainpath="${1%/}"
 domainpath="${domainpath:-$PWD}"
 domainpath=$(cd "$domainpath"; echo "$PWD")
 
-newest=$(find "$domainpath" -type f | sort | tail -1)
+newest=$(find "$domainpath" -type f -name '*.har' | sort | tail -1)
 
 if [[ -e "$newest" ]]; then
 	echo "$newest"
