3bce2dbcf28670a0a1770cc93d978c40110e3e7f
Joel Purra <code+github@joelpurra.com>
2014-07-21 12:10:21 +0200

    Filter HAR data on failure/success of the initial request

---
+++ src/extract/errors/successful-page-loads.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+set -e
+
+read -d '' selectSuccessfulPageLoad <<-'EOF' || true
+def isFailedPageLoad:
+	.log
+	| (
+		(
+			.entries
+			| (
+				type == "null"
+				or
+				length == 0
+			)
+		)
+		or
+		(
+			.entries[0]
+			| (
+				(
+					.response
+					| type == "null"
+				)
+				or
+				(
+					.response.status
+					| (
+						type == "null"
+						or
+						. <= 0
+					)
+				)
+			)
+		)
+	);
+
+select(!isFailedPageLoad)
+EOF
+
+cat | jq "$selectSuccessfulPageLoad"
