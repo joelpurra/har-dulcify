090d5aaa7cb562c80aafd9dae2938b291cfd3aef
Joel Purra <code+github@joelpurra.com>
2014-07-24 14:21:09 +0200

    Create disconnect dataset analysis as part of preparations

--- src/one-shot/preparations.sh
+++ src/one-shot/preparations.sh
@@ -4,8 +4,10 @@ set -e
 # services.json (also known as disconnect-plaintext.json) from disconnect.me
 # Alternative URL: https://github.com/disconnectme/disconnect/raw/master/firefox/content/disconnect.safariextension/opera/chrome/data/services.json
 wget "https://services.disconnect.me/disconnect-plaintext.json"
-cat "disconnect-plaintext.json" | "${BASH_SOURCE%/*}/../classification/disconnect/prepare-service-list.sh" > "prepared.disconnect.services.json"
+<"disconnect-plaintext.json" "${BASH_SOURCE%/*}/../classification/disconnect/prepare-service-list.sh" > "prepared.disconnect.services.json"
+<"prepared.disconnect.services.json" "${BASH_SOURCE%/*}/../classification/disconnect/analysis.sh" > "prepared.disconnect.services.analysis.json"
+
 
 wget "https://publicsuffix.org/list/effective_tld_names.dat"
-cat "effective_tld_names.dat" | "${BASH_SOURCE%/*}/../classification/effective-tld/prepare-list.sh" > "prepared.effective-tld.json"
+<"effective_tld_names.dat" "${BASH_SOURCE%/*}/../classification/effective-tld/prepare-list.sh" > "prepared.effective-tld.json"
 
