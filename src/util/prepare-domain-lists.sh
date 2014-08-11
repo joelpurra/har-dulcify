25dc6272f5ad5bf386c1763ec48f0f7e24dfc319
Joel Purra <code+github@joelpurra.com>
2014-08-11 21:15:57 +0200

    Also create inverted domain lists, for domain to group lookups

--- src/util/prepare-domain-lists.sh
+++ src/util/prepare-domain-lists.sh
@@ -19,6 +19,8 @@ set -e
 #  */unique/	Unique name for all text files in the folder.
 #  */json/	A JSON object with the file name as property name, and an array of all values.
 #  */json/merged/	All JSON files/objects in the folder merged into one.
+#  */json-inverted/	Domain names as properties, groups (text file names) as values.
+#  */json-inverted/merged/	Merged inverted, with domain names as properties and groups/arrays of groups as values.
 #  */stats/	Line counts, total counts.
 
 [[ ! `which idn` ]] && { echo "idn is required"; exit 1; }
@@ -43,19 +45,160 @@ removeWhitespaceAndEmptyLines() {
 	sedExtRegexp -e 's/[[:space:]]//g' -e '/^$/d'
 }
 
+read -d '' invertedObject <<-'EOF' || true
+def toNullOrSingleValueOrArray:
+	if length == 0 then
+		# Replace an empty array with null.
+		null
+	elif length == 1 then
+		# Replace an array with a single element with that element.
+		.[0]
+	else
+		# Return an array with more than one element as is.
+		.
+	end;
+
+def mergeArrayOfObjectsToObjectWithDuplicatesAsArray:
+	reduce .[] as $obj (
+		{};
+		. as $big
+		| $obj
+		| to_entries
+		| .[]
+		| .key as $key
+		| .value as $value
+		| $big
+		| if $big | has($key) then
+			$big[$key] += [ $value ]
+		else
+			$big[$key] = [ $value ]
+		end
+	);
+
+def flattenObject:
+	[
+		to_entries
+		| .[]
+		| .key as $category
+		| .value[]
+		| . as $domain
+		| {
+			domain: $domain,
+			category: $category,
+		}
+	];
+
+def groupDomains:
+	group_by(.domain)
+	| map(
+		{
+			# Create a property out of the domain.
+			(.[0].domain):
+				# Merge deeper properties to (arrays of) unique values.
+				[ .[].category ] | unique | toNullOrSingleValueOrArray
+		}
+	);
+
+def transformRawDomainList:
+	flattenObject
+	| groupDomains
+	| mergeArrayOfObjectsToObjectWithDuplicatesAsArray;
+
+transformRawDomainList
+| with_entries(
+	.value |= toNullOrSingleValueOrArray
+)
+EOF
+
+read -d '' mergeToObject <<-'EOF' || true
+def toNullOrSingleValueOrArray:
+	if length == 0 then
+		# Replace an empty array with null.
+		null
+	elif length == 1 then
+		# Replace an array with a single element with that element.
+		.[0]
+	else
+		# Return an array with more than one element as is.
+		.
+	end;
+
+def mergeArrayOfObjectsToObject:
+	# Assumes that the array's objects have unique enough properties to be suitable for merging.
+	reduce .[] as $obj ({}; . + $obj);
+
+mergeArrayOfObjectsToObject
+| with_entries(
+	.value |= toNullOrSingleValueOrArray
+)
+EOF
+
+read -d '' mergeToObjectWithDuplicatesAsArray <<-'EOF' || true
+def toNullOrSingleValueOrArray:
+	if length == 0 then
+		# Replace an empty array with null.
+		null
+	elif length == 1 then
+		# Replace an array with a single element with that element.
+		.[0]
+	else
+		# Return an array with more than one element as is.
+		.
+	end;
+
+def splitObjectToObjects:
+	to_entries
+	| .[]
+	| {
+		(.key): .value
+	};
+
+# TODO: mention that this function only merges a single object property (the last .key, alphabetically).
+# Why? Because ". as $big" creates a reference to a point in time. Only the last modified version will be returned.
+def mergeArrayOfObjectsToObjectWithDuplicatesAsArray:
+	reduce .[] as $obj (
+		{};
+		. as $big
+		| $obj
+		| to_entries
+		| .[]
+		| .key as $key
+		| .value as $value
+		| $big
+		| if $big | has($key) then
+			$big[$key] += [ $value ]
+		else
+			$big[$key] = [ $value ]
+		end
+	);
+
+# HACK: splitObjectToObjects to get over mergeArrayOfObjectsToObjectWithDuplicatesAsArray limitations.
+map(splitObjectToObjects)
+| mergeArrayOfObjectsToObjectWithDuplicatesAsArray
+| with_entries(
+	.value |= toNullOrSingleValueOrArray
+)
+EOF
+
 createJsonVersions() {
 	mkdir -p "json"
+	mkdir -p "json-inverted"
 
 	while IFS= read -r -d '' file;
 	do
 		name=$(basename -a -s ".txt" "$file")
 		cat "$file" | jq --raw-input --slurp --arg name "$name" '{ ($name): split("\n") }' > "json/$name.json"
+		<"json/$name.json" jq "$invertedObject" > "json-inverted/$name.inverted.json"
 	done < <(find '.' -depth 1 -type f -name '*.txt' -print0)
 
 	cd "json"
 	mkdir -p "merged"
-	# mergeArrayOfObjectsToObject
-	cat *.json | jq --slurp 'reduce .[] as $obj ({}; . + $obj)' > "merged/merged.json"
+	cat *.json | jq --slurp "$mergeToObject" > "merged/merged.json"
+	cd ..
+
+	cd "json-inverted"
+	mkdir -p "merged"
+	cat *.json | jq --slurp "$mergeToObjectWithDuplicatesAsArray" > "merged/merged.duplicates.json"
 	cd ..
 }
 
