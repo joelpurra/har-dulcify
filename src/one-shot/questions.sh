dfa990c54f39fc8eaa5c076308ff9ef0d1ea9924
Joel Purra <code+github@joelpurra.com>
2014-08-21 07:37:38 +0200

    Use minimal input data level

--- src/one-shot/questions.sh
+++ src/one-shot/questions.sh
@@ -2,6 +2,6 @@
 set -e
 
 # Google Tag Manager
-<"domains.parts.expanded.classified.disconnect.effective-tld.json" "${BASH_SOURCE%/*}/../util/parallel-chunks.sh" "${BASH_SOURCE%/*}/../questions/google-gtm-ga-dc.sh" > "google-gtm-ga-dc.json"
+<"domains.parts.expanded.json" "${BASH_SOURCE%/*}/../util/parallel-chunks.sh" "${BASH_SOURCE%/*}/../questions/google-gtm-ga-dc.sh" > "google-gtm-ga-dc.json"
 # TODO: parallelize question aggregation?
 <"google-gtm-ga-dc.json" "${BASH_SOURCE%/*}/../questions/google-gtm-ga-dc.aggregate.sh" > "google-gtm-ga-dc.aggregate.json"
