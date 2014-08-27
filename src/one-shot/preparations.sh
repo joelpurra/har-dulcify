3270bf0349b92871b2c6f0c2069af15b69241c04
Joel Purra <code+github@joelpurra.com>
2014-08-27 14:35:42 +0200

    Use a simplified progress bar when downloading third-party datasets

--- src/one-shot/preparations.sh
+++ src/one-shot/preparations.sh
@@ -1,13 +1,21 @@
 #!/usr/bin/env bash
 set -e
 
+# Fallback to wget, which is chattier or doesn't display a progress bar?
+[[ -z "$(which curl)" ]] && { echo "curl is required" >&2; exit 1; }
+
+download() {
+	echo "Downloading $1" >&2
+	curl --progress-bar -O "$1"
+}
+
 # services.json (also known as disconnect-plaintext.json) from disconnect.me
 # Alternative URL: https://github.com/disconnectme/disconnect/raw/master/firefox/content/disconnect.safariextension/opera/chrome/data/services.json
-wget "https://services.disconnect.me/disconnect-plaintext.json"
+download "https://services.disconnect.me/disconnect-plaintext.json"
 <"disconnect-plaintext.json" "${BASH_SOURCE%/*}/../classification/disconnect/prepare-service-list.sh" > "prepared.disconnect.services.json"
 <"prepared.disconnect.services.json" "${BASH_SOURCE%/*}/../classification/disconnect/analysis.sh" > "prepared.disconnect.services.analysis.json"
 
 
-wget "https://publicsuffix.org/list/effective_tld_names.dat"
+download "https://publicsuffix.org/list/effective_tld_names.dat"
 <"effective_tld_names.dat" "${BASH_SOURCE%/*}/../classification/effective-tld/prepare-list.sh" > "prepared.effective-tld.json"
 
