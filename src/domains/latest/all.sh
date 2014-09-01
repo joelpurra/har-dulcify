70a58002898ab34d6601c700c68347746fa3ad90
Joel Purra <code+github@joelpurra.com>
2014-09-01 09:57:02 +0200

    Fix find path exclusion by reordering the arguments

--- src/domains/latest/all.sh
+++ src/domains/latest/all.sh
@@ -6,4 +6,4 @@ domainroot="${domainroot:-$PWD}"
 domainroot=$(cd -- "$domainroot"; echo "$PWD")
 
 # TODO: rewrite to work on strings from find -type f and split/group on directory.
-find "$domainroot" -type d -print0 ! -path "$domainroot" | parallel --jobs 10 --null --line-buffer "${BASH_SOURCE%/*}/single.sh" "{}"
+find "$domainroot" ! -path "$domainroot" -type d -print0 | parallel -N 1 --jobs 10 --null --line-buffer "${BASH_SOURCE%/*}/single.sh" "{}"
