9d6f88b05202c595ff3315ab756e6423781636b2
Joel Purra <code+github@joelpurra.com>
2014-09-22 12:37:18 +0200

    Run all multiset queries

---
+++ src/one-shot/multiset.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -e
+
+(( "$#" == 0 )) && { echo "Usage: "$(basename "$BASH_SOURCE")" <dataset folder path>[ <dataset folder path>[ <dataset folder path>]]" 1>&2; exit 1; }
+
+"${BASH_SOURCE%/*}/../multiset/download-retries.sh" "$@"
+"${BASH_SOURCE%/*}/../multiset/non-failed.classification.domain-scope.coverage.sh" "$@"
+"${BASH_SOURCE%/*}/../multiset/non-failed.classification.secure.coverage.sh" "$@"
+"${BASH_SOURCE%/*}/../multiset/non-failed.disconnect.categories.sh" "$@"
+"${BASH_SOURCE%/*}/../multiset/non-failed.disconnect.domains.sh" "$@"
+"${BASH_SOURCE%/*}/../multiset/non-failed.disconnect.organizations.sh" "$@"
+"${BASH_SOURCE%/*}/../multiset/non-failed.url.counts.sh" "$@"
