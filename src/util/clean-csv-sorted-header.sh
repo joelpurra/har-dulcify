29ad24acb6d25f8aea028bed67c7b36f56bd4ccf
Joel Purra <code+github@joelpurra.com>
2014-10-31 13:43:42 +0100

    Allow more than two-digit columns in tables

--- src/util/clean-csv-sorted-header.sh
+++ src/util/clean-csv-sorted-header.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -e
 
-cat | sed -e '1s/^"[[:digit:]][[:digit:]]--/"/g' -e '1s/,"[[:digit:]][[:digit:]]--/,"/g'
+cat | sed -e '1s/^"[[:digit:]][[:digit:]][[:digit:]]*--/"/g' -e '1s/,"[[:digit:]][[:digit:]][[:digit:]]*--/,"/g'
