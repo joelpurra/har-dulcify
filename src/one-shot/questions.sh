eb29e3eab1a83404aa86a83194958929d66ae592
Joel Purra <code+github@joelpurra.com>
2014-11-10 16:31:01 +0100

    Run ratio-buckets with the other questions

--- src/one-shot/questions.sh
+++ src/one-shot/questions.sh
@@ -11,5 +11,10 @@ set -e
 # TODO: parallelize question aggregation?
 <"origin-redirects.json" "${BASH_SOURCE%/*}/../questions/origin-redirects.aggregate.sh" > "origin-redirects.aggregate.json"
 
+# Ratio buckets
+<"domains.parts.expanded.public-suffix.classified.disconnect.json" "${BASH_SOURCE%/*}/../util/parallel-chunks.sh" "${BASH_SOURCE%/*}/../questions/ratio-buckets.sh" > "ratio-buckets.json"
+# TODO: parallelize question aggregation?
+<"ratio-buckets.json" "${BASH_SOURCE%/*}/../questions/ratio-buckets.aggregate.sh" > "ratio-buckets.aggregate.json"
+
 # Regroup disconnect's categories and organizations
 <"aggregates.analysis.json" "${BASH_SOURCE%/*}/../questions/disconnect.categories.organizations.sh" "prepared.disconnect.services.json" > "aggregate.disconnect.categories.organizations.json"
