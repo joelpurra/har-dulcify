7da5075239c5bc570f003d91a9516f8e43de623b
Joel Purra <code+github@joelpurra.com>
2015-02-05 10:16:37 +0100

    datasets.non-failed.domains.ratios.sorted.tsv

--- src/multiset/non-failed.requests.counts.sh
+++ src/multiset/non-failed.requests.counts.sh
@@ -9,10 +9,14 @@ read -d '' getNonFailedUrlCounts <<-'EOF' || true
 	"non-failed-domains": .successfulOrigin.origin.counts.count,
 	"non-failed-domains-with-internal-requests": .successfulOrigin.internalUrls.requestedUrlsDistinct.counts.countDistinct,
 	"non-failed-domains-with-external-requests": .successfulOrigin.externalUrls.requestedUrlsDistinct.counts.countDistinct,
+	# TODO: use a proper count instead of primary domain counter object lengths.
+	externalDomains: (.successfulOrigin.externalUrls.requestedUrls.counts.urls.domains | length),
+	externalPrimaryDomains: (.successfulOrigin.externalUrls.requestedUrls.counts.urls."primary-domain" | length),
 	unfilteredRequests: .successfulOrigin.unfilteredUrls.requestedUrls.counts.count,
 	internalRequests: .successfulOrigin.internalUrls.requestedUrls.counts.count,
 	externalRequests: .successfulOrigin.externalUrls.requestedUrls.counts.count,
 	# TODO: use a proper count instead of adding domain counts.
+	externalRequestsDisconnectDomains: (.successfulOrigin.externalUrls.requestedUrls.counts.blocks.disconnect.domains | length),
 	externalRequestsDisconnectMatches: (.successfulOrigin.externalUrls.requestedUrls.counts.blocks.disconnect.domains | add),
 }
 EOF
@@ -23,6 +27,15 @@ read -d '' mapData <<-'EOF' || true
 	"non-failed-domains",
 	"non-failed-domains-with-internal-requests",
 	"non-failed-domains-with-external-requests",
+	externalDomains,
+	externalPrimaryDomains,
+	externalRequestsDisconnectDomains,
+	externalDisconnectDomainsDiffExternalDomains: (.externalDomains - .externalRequestsDisconnectDomains),
+	externalDisconnectDomainsDiffExternalPrimaryDomains: (.externalPrimaryDomains - .externalRequestsDisconnectDomains),
+	externalDisconnectDomainsPerExternalDomains: (.externalRequestsDisconnectDomains / .externalDomains),
+	externalDisconnectDomainsPerExternalPrimaryDomains: (.externalRequestsDisconnectDomains / .externalPrimaryDomains),
+	externalDisconnectDomainsMismatchPerExternalDomains: (1 - (.externalRequestsDisconnectDomains / .externalDomains)),
+	externalDisconnectDomainsMismatchPerExternalPrimaryDomains: (1 - (.externalRequestsDisconnectDomains / .externalPrimaryDomains)),
 	withInternalRatio: (."non-failed-domains-with-internal-requests" / ."non-failed-domains"),
 	withExternalRatio: (."non-failed-domains-with-external-requests" / ."non-failed-domains"),
 	unfilteredRequests,
@@ -48,10 +61,13 @@ map(
 		"02--Domains": ."non-failed-domains",
 		"03--w/ int": ."non-failed-domains-with-internal-requests",
 		"04--w/ ext": ."non-failed-domains-with-external-requests",
-		"05--All requests": .unfilteredRequests,
-		"06--Int": .internalRequests,
-		"07--Ext": .externalRequests,
-		"08--Disco.": .externalRequestsDisconnectMatches,
+		"05--Ext dom.": .externalDomains,
+		"06--Ext prim.": .externalPrimaryDomains,
+		"07--Ext D dom.": .externalRequestsDisconnectDomains,
+		"08--All requests": .unfilteredRequests,
+		"09--Int": .internalRequests,
+		"10--Ext": .externalRequests,
+		"11--Disco.": .externalRequestsDisconnectMatches,
 	}
 )
 EOF
@@ -76,6 +92,24 @@ map(
 )
 EOF
 
+read -d '' renameForTsvColumnOrderingDomainRatios <<-'EOF' || true
+map(
+	{
+		"01--Dataset": .dataset,
+		"02--Domains": ."non-failed-domains",
+		"03--Ext dom.": .externalDomains,
+		"04--Prim.": .externalPrimaryDomains,
+		"05--D dom.": .externalRequestsDisconnectDomains,
+		"06--D diff ext.": .externalDisconnectDomainsDiffExternalDomains,
+		"07--D diff prim.": .externalDisconnectDomainsDiffExternalPrimaryDomains,
+		"08--D/ext.": .externalDisconnectDomainsPerExternalDomains,
+		"09--Prim. det.": .externalDisconnectDomainsPerExternalPrimaryDomains,
+		# "1xxx--D/ext.": .externalDisconnectDomainsMismatchPerExternalDomains,
+		"10--Undet.": .externalDisconnectDomainsMismatchPerExternalPrimaryDomains,
+	}
+)
+EOF
+
 read -d '' sortObjects <<-'EOF' || true
 sort_by(.dataset)
 EOF
@@ -87,3 +121,4 @@ EOF
 # Two output files.
 <"datasets.non-failed.requests.counts.sorted.json" jq "$renameForTsvColumnOrderingCounts" | "${BASH_SOURCE%/*}/../util/array-of-objects-to-tsv.sh" | "${BASH_SOURCE%/*}/../util/clean-tsv-sorted-header.sh" >"datasets.non-failed.requests.counts.sorted.tsv"
 <"datasets.non-failed.requests.counts.sorted.json" jq "$renameForTsvColumnOrderingRatios" | "${BASH_SOURCE%/*}/../util/array-of-objects-to-tsv.sh" | "${BASH_SOURCE%/*}/../util/clean-tsv-sorted-header.sh" >"datasets.non-failed.requests.ratios.sorted.tsv"
+<"datasets.non-failed.requests.counts.sorted.json" jq "$renameForTsvColumnOrderingDomainRatios" | "${BASH_SOURCE%/*}/../util/array-of-objects-to-tsv.sh" | "${BASH_SOURCE%/*}/../util/clean-tsv-sorted-header.sh" >"datasets.non-failed.domains.ratios.sorted.tsv"
